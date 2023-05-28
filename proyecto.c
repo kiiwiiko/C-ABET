164f290f6117987c1182252c05060e20f1146177
Arie Quimi <trr.2002j@gmail.com>
2023-05-28 13:16:54 -0500

    Imprimir

--- proyecto.c
+++ proyecto.c
@@ -1,16 +1,30 @@
+/*En una Institución educativa se almacenan datos sobre el número de alumnos que han ingresado a sus
+diferentes carreras en los dos semestres anuales y en los últimos 5 años. Cada carrera tiene un 
+numero asociado que la identifica: 1. Ingeniería de Software. 2. Administración. 3. Economía.
+4. Relaciones Internacionales. 5. Matemáticas. 6. Contabilidad. 7. Ingeniería Industrial.
+Escriba un programa que calcule:
+a) El año en que ingresó la mayor cantidad de alumnos a la universidad.
+b) La Carrera que recibió la mayor cantidad de alumnos en el último año.
+c) ¿En qué año la carrera de Ingeniería de Software recibió la mayor cantidad de alumnos?*/
+/*Anthony Almeida-Jeison Quimi-Romina Villavicencio*/
+
 #include <stdio.h>
 #include <locale.h>
 #include <time.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    //Se ocupa una funcion para identificar el idioma e imprima bien lo que se desea ver en la ejecucion 
     setlocale(LC_ALL, "Spanish");
+    //se utiliza srand para cada vez que se corre salgan numeros diferentes
     srand(time(NULL));
+     //se declara variables por literales para mayor entendimiento
     int suma, ingresos = 0, anio = 0;
     int alumnos = 0, carrera = 0;
     int sof = 0, anios = 0;
     int car[6][8];
 
+    //se crea un ciclo anidado con el año y la carrera para determinar el ingreso de estudiantes
     for (int i = 1; i < 6; i++) {
         suma = 0;
         for (int j = 1; j < 8; j++) {
@@ -27,7 +41,7 @@ int main(int argc, char *argv[]) {
                 anios = i;
             }
         }
-
+    //se crea un if para el literal a, determinando en que año hubo mas ingresos de estudiantes
         if (suma > ingresos) {
             ingresos = suma;
             anio = i;
@@ -40,6 +54,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
+//se imprime los resultados de los literales
     printf("a) El año en que ingresó la mayor cantidad de alumnos a la universidad fue el #%d con %d estudiantes\n", anio, ingresos);
     printf("b) La Carrera que recibió la mayor cantidad de alumnos en el último año fue la carrera #%d con %d estudiantes\n", carrera, alumnos);
     printf("LEYENDA DE CARRERAS:\n");
